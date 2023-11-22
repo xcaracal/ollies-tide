43c046b85d718889ad96ba934f331194199e9f82
Steven Hoang <hoangste22@gmail.com>
2023-11-22 01:23:19 -0800

    implemented hitbox w/ healthbar

---
+++ main/collision_groups.lua
@@ -0,0 +1,9 @@
+-- collision_groups.lua
+local groups = {
+	player = hash("player_group"),
+	enemy = hash("enemy_group"),
+	--obstacle = hash("obstacle_group"),
+	-- Add more collision groups as needed
+}
+
+return groups
