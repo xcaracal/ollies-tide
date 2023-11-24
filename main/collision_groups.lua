97dfec03cc0e1b021a706dbbc801097141b2558c
Steven Hoang <hoangste22@gmail.com>
2023-11-23 17:43:15 -0800

    added a damage number var and added to code

--- main/collision_groups.lua
+++ main/collision_groups.lua
@@ -1,7 +1,7 @@
 -- collision_groups.lua
 local groups = {
-	player = hash("player_group"),
-	enemy = hash("enemy_group"),
+	PLAYER_GROUP = hash("PLAYER_GROUP"),
+	ENEMY_GROUP = hash("ENEMY_GROUP"),
 	--obstacle = hash("obstacle_group"),
 	-- Add more collision groups as needed
 }
