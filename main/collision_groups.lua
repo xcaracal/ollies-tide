a5734e7281d5470afcaee07805239c0c7e6ad769
Steven Hoang <hoangste22@gmail.com>
2023-11-29 13:24:58 -0800

    added collision boxes for attack animation

--- main/collision_groups.lua
+++ main/collision_groups.lua
@@ -2,6 +2,8 @@
 local groups = {
 	PLAYER_GROUP = hash("PLAYER_GROUP"),
 	ENEMY_GROUP = hash("ENEMY_GROUP"),
+	PLAYER_ATTACK_GROUP = hash("PLAYER_ATTACK_GROUP"),
+	ENEMY_ATTACK_GROUP = hash("ENEMY_ATTACK_GROUP")
 	--obstacle = hash("obstacle_group"),
 	-- Add more collision groups as needed
 }
